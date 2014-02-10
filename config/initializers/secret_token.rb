b8152fa9809926c246f161b96f50d99adf64f32a
Joshua Farrar <joshua.farrar@charity.org>
2014-02-10 10:16:29 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsAngularBootstrapSeed::Application.config.secret_key_base = '5cb2777ed4c8fe2b6f81ccbbaaee2b7d949a1d0441599ffa2c6b155ed92b2678db597f7037663516f4f63baf0ba3e8fc9d17f6374570fd2561f1a7759d913301'
